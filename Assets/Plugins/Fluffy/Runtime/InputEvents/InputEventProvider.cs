e8afed277324ce03268e9c9d241eada52fb7afb1
TarobeWanwanLand <99389252+TarobeWanwanLand@users.noreply.github.com>
2023-05-21 13:34:06 +0900

    [add] LINQの拡張とポインター入力イベントの実装

---
+++ Assets/Plugins/Fluffy/Runtime/InputEvents/InputEventProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace Fluffy.InputEvents
+{
+    public static class InputEvent
+    {
+        private static Pointer _pointer;
+        public static Pointer Pointer => _pointer ??= new();
+    }
+}
---
+++ Assets/Plugins/Fluffy/Runtime/Linq/Buffer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fluffy.Linq
+{
+    public static partial class LinqExtensions
+    {
+        public static IEnumerable<IEnumerable<T>> Buffer<T>(this IEnumerable<T> source, int count, int skip)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (count <= 0)
+                throw new ArgumentOutOfRangeException(nameof(count));
+            if (skip <= 0)
+                throw new ArgumentOutOfRangeException(nameof(skip));
+
+            using var e = source.GetEnumerator();
+            
+            var queue = new Queue<T>(count);
+
+            do
+            {
+                var value = e.Current;
+                queue.Enqueue(value);
+
+                if (queue.Count == count)
+                {
+                    yield return queue;
+
+                    for (var i = 0; i < skip; i++)
+                    {
+                        queue.Dequeue();
+                    }
+                }
+            } while (e.MoveNext());
+        }
+    }
+}
